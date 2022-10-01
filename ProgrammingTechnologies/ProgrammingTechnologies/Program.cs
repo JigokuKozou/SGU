642f35adb4093999beeeecde6e979a2f3b448555
JigokuKozou <secretplayer@inbox.ru>
2022-10-01 23:12:17 +0400

    Refactoring: Now BuilderImpl is a class library and ProgrammingTechnologies is a startup project

---
+++ ProgrammingTechnologies/ProgrammingTechnologies/Program.cs
@@ -0,0 +1,16 @@
+﻿using BuilderImpl;
+using System.Drawing.Imaging;
+
+namespace ProgrammingTechnologies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KitchenRoom kitchen = new KitchenRoom(12, 12);
+            Apartment apartment = new Apartment(kitchen);
+            apartment.GenerateRoom();
+            kitchen.GetRoom(42).Save("out.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
