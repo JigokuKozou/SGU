e01a205c533670046c1874e18f1db3f593a2b1a5
JigokuKozou <secretplayer@inbox.ru>
2022-10-01 23:37:25 +0400

    added Singleton implementation

---
+++ ProgrammingTechnologies/Singleton/Sound.cs
@@ -0,0 +1,11 @@
+﻿namespace Singleton
+{
+    public class Sound
+    {
+        static Sound sound;
+
+        protected Sound() { }
+
+        public static Sound Instance => sound is null ? new Sound() : sound;
+    }
+}
