a5d95d5b204382c38683a59a87704a4a54450ff8
JigokuKozou <secretplayer@inbox.ru>
2022-10-02 01:56:10 +0400

    Added: demo method display status info

--- ProgrammingTechnologies/ProgrammingTechnologies/Program.cs
+++ ProgrammingTechnologies/ProgrammingTechnologies/Program.cs
@@ -1,6 +1,7 @@
-﻿using Adapter;
-using Builder;
+﻿using Builder;
+using Adapter;
 using Decorator;
+using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 
@@ -10,31 +11,64 @@ namespace ProgrammingTechnologies
     {
         static void Main(string[] args)
         {
+            int scale = 42;
+            int width = 12;
+            int height = 12;
+
+            StartBuilderDemo(scale, width, height);
+
+            StartAdapterDemo(scale, width, height);
+
             StartDecoratorDemo();
         }
 
         static void StartBuilderDemo(int scale, int width, int height)
         {
+
+
+            Console.WriteLine("Builder demo working...");
+
             ArrayRoom kitchen = new KitchenRoom(width, height);
             Apartment apartment = new Apartment(kitchen);
+
             apartment.GenerateRoom();
-            kitchen.GetRoom(scale).Save("outBuilder.jpg", ImageFormat.Jpeg);
+
+            string pathOutput = "outBuilder.jpg";
+            kitchen.GetRoom(scale).Save(pathOutput, ImageFormat.Jpeg);
+
+            Console.WriteLine("Builder demo finished!" + Environment.NewLine +
+                "File output: " + pathOutput + Environment.NewLine);
         }
 
         static void StartAdapterDemo(int scale, int width, int height)
         {
+            Console.WriteLine("Adapter demo working...");
+
             ArrayRoom basement = new BasementAdapter(width, height);
             Apartment apartment = new Apartment(basement);
+
             apartment.GenerateRoom();
-            basement.GetRoom(scale).Save("outAdapter.jpg", ImageFormat.Jpeg);
+
+            string pathOutput = "outAdapter.jpg";
+            basement.GetRoom(scale).Save(pathOutput, ImageFormat.Jpeg);
+
+            Console.WriteLine("Adapter demo finished!" + Environment.NewLine +
+                "File output: " + pathOutput + Environment.NewLine);
         }
 
         static void StartDecoratorDemo(string pathImage = "outBuilder.jpg")
         {
-            Bitmap bitmap = new Bitmap(pathImage);
-            EditorOfBitmap editor = new GrayscaleConverter(new ReflectorHorizontallyBitmap(new ReflectorVerticalBitmap()));
+            Console.WriteLine("Decorator demo working...");
 
-            editor.GetFormattedBitmap(bitmap).Save("decorated" + pathImage, ImageFormat.Jpeg);
+            Bitmap bitmap = new Bitmap(pathImage);
+            EditorOfBitmap editor = 
+                new GrayscaleConverter(new ReflectorHorizontallyBitmap(new ReflectorVerticalBitmap()));
+
+            string pathOutput = "decorated_" + pathImage;
+            editor.GetFormattedBitmap(bitmap).Save(pathOutput, ImageFormat.Jpeg);
+
+            Console.WriteLine("Decorator demo finished!" + Environment.NewLine +
+                "File output: " + pathOutput + Environment.NewLine);
         }
     }
 }
