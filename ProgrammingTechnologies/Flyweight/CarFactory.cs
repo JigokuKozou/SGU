16caf546bb3edf4e46a09600913191712fc209c1
JigokuKozou <secretplayer@inbox.ru>
2022-10-03 14:15:32 +0400

    Added: Flyweight implementation

---
+++ ProgrammingTechnologies/Flyweight/CarFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flyweight
+{
+    public enum CarType
+    {
+        FordMustang,
+        SkodaRapid,
+    }
+
+    public class CarFactory
+    {
+        private readonly IDictionary<CarType, ICar> cars = new Dictionary<CarType, ICar>();
+
+        public ICar GetCar(CarType type)
+        {
+            if (cars.ContainsKey(type)) return cars[type];
+
+            ICar car = CreateCar(type);
+            cars.Add(type, car);
+
+            return car;
+        }
+
+        private ICar CreateCar(CarType type)
+        {
+            ICar car;
+
+            switch (type)
+            {
+                case CarType.FordMustang:
+                    car = new FordMustang(); break;
+                case CarType.SkodaRapid:
+                    car = new SkodaRapid(); break;
+                default:
+                    throw new ArgumentException("Invalid choise");
+            }
+
+            return car;
+        }
+    }
+}
---
+++ ProgrammingTechnologies/Flyweight/ICar.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+
+namespace Flyweight
+{
+    public interface ICar
+    {
+        string Engine { get; }
+
+        ConsoleColor Color { get; }
+
+        void SetLocation(int x, int y);
+    }
+
+    public abstract class Car : ICar
+    {
+        public Car()
+        {
+            Console.WriteLine(ToString() + " создан в памяти");
+        }
+
+        public abstract string Engine { get; }
+
+        public abstract ConsoleColor Color { get; }
+
+        public virtual void SetLocation(int x, int y)
+        {
+            ConsoleColor previous = Console.ForegroundColor;
+
+            Console.ForegroundColor = Color;
+            Console.WriteLine($"{ToString()} назначена точка ({x}, {y})");
+            Console.ForegroundColor = previous;
+        }
+
+        public override string ToString()
+        {
+            return $"[Двигатель: {Engine}]";
+        }
+    }
+
+    public class FordMustang : Car
+    {
+        public override string Engine => "315 DOHC Voodoo V8 5.2 GT350";
+
+        public override ConsoleColor Color => ConsoleColor.Red;
+
+        public override string ToString()
+        {
+            return "Ford Mustang " + base.ToString();
+        }
+    }
+
+    public class SkodaRapid : Car
+    {
+        public override string Engine => "1.6 л, 90 л.с., бензин";
+
+        public override ConsoleColor Color => ConsoleColor.Blue;
+
+        public override string ToString()
+        {
+            return "Skoda Rapid " + base.ToString();
+        }
+    }
+}
+
