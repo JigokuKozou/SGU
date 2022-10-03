cc21b786a76ee593ec0c80d6af7ed6ea7c34d74c
JigokuKozou <secretplayer@inbox.ru>
2022-10-03 18:29:59 +0400

    Added: Strategy implementation

---
+++ ProgrammingTechnologies/Patterns/Behavioral/Strategy/Hero.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class Hero
+    {
+        private readonly string name;
+
+        private IWeapon weapon;
+
+        public Hero(string name) => this.name = name;
+
+        public void Take(IWeapon weapon)
+        {
+            if (weapon is null)
+                throw new ArgumentNullException(nameof(weapon));
+
+            this.weapon = weapon;
+
+            Console.WriteLine(name + " взял " + weapon.GetType().Name);
+        }
+
+        public void Attack()
+        {
+            if (weapon is null)
+            {
+                Console.WriteLine(name + " не может аттаковать. Необходимо оружие.");
+                return;
+            }
+
+            Console.Write(name + " ");
+            weapon.Attack();
+        }
+    }
+}
---
+++ ProgrammingTechnologies/Patterns/Behavioral/Strategy/IGun.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Strategy
+{
+    public interface IWeapon
+    {
+        void Attack();
+    }
+
+    public class Crowbar : IWeapon
+    {
+        public void Attack()
+        {
+            Console.WriteLine("ударил монтировкой");
+        }
+    }
+
+    public class Pistol : IWeapon
+    {
+        public void Attack()
+        {
+            Console.WriteLine("выстрелил из пистолета");
+        }
+    }
+}
